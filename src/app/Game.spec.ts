f61574012b30a2af5aa9f2d3afb978b52e62b3cb
Izhaki <roey@gefri.org>
2017-02-13 23:02:39 +0000

    Refactor: Indent tests that apply after all players have joined.

--- src/app/Game.spec.ts
+++ src/app/Game.spec.ts
@@ -8,17 +8,6 @@ import {
 
 describe( 'Game', () => {
 
-    beforeEach( () => {
-        this.game = new Game();
-        this.game.addPlayer( 'Johnny' );
-        this.game.addPlayer( 'Depp' );
-        this.game.startNewMatch();
-        this.events = [];
-        this.game.subscribe( aEvent => {
-            this.events.push( aEvent );
-        });
-    });
-
     describe( 'generateRandomChoice()', () => {
 
         beforeEach( () => {
@@ -51,116 +40,131 @@ describe( 'Game', () => {
 
     });
 
-    describe( 'A match should conclude with', () => {
+    describe( 'once all players joined', () => {
 
-        describe( 'a draw if the choice of both players chose', () => {
+        beforeEach( () => {
+            this.game = new Game();
+            this.game.addPlayer( 'Johnny' );
+            this.game.addPlayer( 'Depp' );
+            this.game.startNewMatch();
+            this.events = [];
+            this.game.subscribe( aEvent => {
+                this.events.push( aEvent );
+            });
+        });
 
-            it( 'rock', () => {
-                this.game.setPlayerChoice( 0, ROCK );
-                this.game.setPlayerChoice( 1, ROCK );
+        describe( 'A match should conclude with', () => {
 
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( DRAW );
+            describe( 'a draw if the choice of both players chose', () => {
+
+                it( 'rock', () => {
+                    this.game.setPlayerChoice( 0, ROCK );
+                    this.game.setPlayerChoice( 1, ROCK );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( DRAW );
+                });
+
+                it( 'paper', () => {
+                    this.game.setPlayerChoice( 0, PAPER );
+                    this.game.setPlayerChoice( 1, PAPER );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( DRAW );
+                });
+
+                it( 'scissors', () => {
+                    this.game.setPlayerChoice( 0, SCISSORS );
+                    this.game.setPlayerChoice( 1, SCISSORS );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( DRAW );
+                });
+
             });
 
-            it( 'paper', () => {
-                this.game.setPlayerChoice( 0, PAPER );
-                this.game.setPlayerChoice( 1, PAPER );
+            describe( 'player 0 winning when', () => {
 
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( DRAW );
+                it( 'player 0 chose rock and player 1 chose scissors', () => {
+                    this.game.setPlayerChoice( 0, ROCK );
+                    this.game.setPlayerChoice( 1, SCISSORS );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( 0 );
+                });
+
+                it( 'player 0 chose scissors and player 1 chose paper', () => {
+                    this.game.setPlayerChoice( 0, SCISSORS );
+                    this.game.setPlayerChoice( 1, PAPER );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( 0 );
+                });
+
+                it( 'player 0 chose paper and player 1 chose rock', () => {
+                    this.game.setPlayerChoice( 0, PAPER );
+                    this.game.setPlayerChoice( 1, ROCK );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( 0 );
+                });
+
             });
 
-            it( 'scissors', () => {
-                this.game.setPlayerChoice( 0, SCISSORS );
-                this.game.setPlayerChoice( 1, SCISSORS );
+            describe( 'player 1 winning when', () => {
 
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( DRAW );
+                it( 'player 1 chose rock and player 0 chose scissors', () => {
+                    this.game.setPlayerChoice( 1, ROCK );
+                    this.game.setPlayerChoice( 0, SCISSORS );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( 1 );
+                });
+
+                it( 'player 1 chose scissors and player 0 chose paper', () => {
+                    this.game.setPlayerChoice( 1, SCISSORS );
+                    this.game.setPlayerChoice( 0, PAPER );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( 1 );
+                });
+
+                it( 'player 1 chose paper and player 0 chose rock', () => {
+                    this.game.setPlayerChoice( 1, PAPER );
+                    this.game.setPlayerChoice( 0, ROCK );
+
+                    expect( this.events.length ).toEqual( 1 );
+                    expect( this.events[0].winner ).toBe( 1 );
+                });
+
             });
 
         });
 
-        describe( 'player 0 winning when', () => {
+        it( 'may consist of more than one match', () => {
+            this.game.setPlayerChoice( 0, ROCK );
+            this.game.setPlayerChoice( 1, ROCK );
 
-            it( 'player 0 chose rock and player 1 chose scissors', () => {
-                this.game.setPlayerChoice( 0, ROCK );
-                this.game.setPlayerChoice( 1, SCISSORS );
+            this.game.startNewMatch();
 
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( 0 );
-            });
+            this.game.setPlayerChoice( 0, ROCK );
+            this.game.setPlayerChoice( 1, PAPER );
 
-            it( 'player 0 chose scissors and player 1 chose paper', () => {
-                this.game.setPlayerChoice( 0, SCISSORS );
-                this.game.setPlayerChoice( 1, PAPER );
-
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( 0 );
-            });
-
-            it( 'player 0 chose paper and player 1 chose rock', () => {
-                this.game.setPlayerChoice( 0, PAPER );
-                this.game.setPlayerChoice( 1, ROCK );
-
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( 0 );
-            });
-
+            expect( this.events.length ).toEqual( 2 );
+            expect( this.events[0].winner ).toBe( DRAW );
+            expect( this.events[1].winner ).toBe( 1 );
         });
 
-        describe( 'player 1 winning when', () => {
+        it( 'may involve a player in auto mode', () => {
+            spyOn( Math, 'random').and.returnValue( 0 );
 
-            it( 'player 1 chose rock and player 0 chose scissors', () => {
-                this.game.setPlayerChoice( 1, ROCK );
-                this.game.setPlayerChoice( 0, SCISSORS );
+            this.game.setPlayerAutoMode( 1, true );
+            this.game.setPlayerChoice( 0, ROCK );
 
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( 1 );
-            });
-
-            it( 'player 1 chose scissors and player 0 chose paper', () => {
-                this.game.setPlayerChoice( 1, SCISSORS );
-                this.game.setPlayerChoice( 0, PAPER );
-
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( 1 );
-            });
-
-            it( 'player 1 chose paper and player 0 chose rock', () => {
-                this.game.setPlayerChoice( 1, PAPER );
-                this.game.setPlayerChoice( 0, ROCK );
-
-                expect( this.events.length ).toEqual( 1 );
-                expect( this.events[0].winner ).toBe( 1 );
-            });
-
+            expect( this.events.length ).toEqual( 1 );
+            expect( this.events[0].winner ).toBe( DRAW );
         });
 
-    });
-
-    it( 'may consist of more than one match', () => {
-        this.game.setPlayerChoice( 0, ROCK );
-        this.game.setPlayerChoice( 1, ROCK );
-
-        this.game.startNewMatch();
-
-        this.game.setPlayerChoice( 0, ROCK );
-        this.game.setPlayerChoice( 1, PAPER );
-
-        expect( this.events.length ).toEqual( 2 );
-        expect( this.events[0].winner ).toBe( DRAW );
-        expect( this.events[1].winner ).toBe( 1 );
-    });
-
-    it( 'may involve a player in auto mode', () => {
-        spyOn( Math, 'random').and.returnValue( 0 );
-
-        this.game.setPlayerAutoMode( 1, true );
-        this.game.setPlayerChoice( 0, ROCK );
-
-        expect( this.events.length ).toEqual( 1 );
-        expect( this.events[0].winner ).toBe( DRAW );
     });
 
 });
